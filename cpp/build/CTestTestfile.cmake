48e5b3b2533d2d6391fa76d9e55a798f532ef09f
IlyaAleichik <ilya.alejchik@outlook.com>
2022-02-28 17:50:56 +0300

    add project files

---
+++ cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/Desktop/cross-alg/cpp
+# Build directory: /home/ilya/Desktop/cross-alg/cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
